51eebe2a7b0b207672a7a8dd73ef3ae76a90a21e
LAPTOP-36C9AG9R\Jayanthan <jayanthandrj@gmail.com>
2023-07-30 19:24:55 +0530

    Correction

--- src/pages/FindStays.jsx
+++ src/pages/FindStays.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-//import Navbar from '../components/Navbar'
-//import Footer from '../components/Footer'
-//import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 import Function from '../components/Function'
 const FindStays = () => {
     return (
         <div>
-           
+            <Navbar></Navbar>
+            <Hero></Hero>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
+            <Footer>Hello</Footer>
             <Function></Function>
         </div>
     )
@@ -14,8 +22,3 @@ const FindStays = () => {
 
 export default FindStays
 // Hello
-
-/*
- <Navbar></Navbar>
-            <Hero></Hero>
-            <Footer></Footer> */
