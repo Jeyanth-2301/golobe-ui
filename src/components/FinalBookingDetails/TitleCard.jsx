dc17ede1a7caa72b2664e403c6d0cbceaed63e48
aarka <aarkachristalsujaag@gmail.com>
2023-08-07 14:42:28 +0530

    Reviews UI without API

--- src/components/FinalBookingDetails/TitleCard.jsx
+++ src/components/FinalBookingDetails/TitleCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 //import React from 'react'
 import { useState, useEffect } from 'react';
 import html2pdf from 'html2pdf.js'
--- src/components/hotel-details/reviews/ReviewForm.jsx
+++ src/components/hotel-details/reviews/ReviewForm.jsx
@@ -1,47 +1,159 @@
-import React, { useState } from "react";
-import {
-  TextField,
-  Typography,
-  Button,
-  Box,
-  Grid,
-  CircularProgress,
-} from "@mui/material";
+/* eslint-disable no-unexpected-multiline */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+// /* eslint-disable react/prop-types */
+// /* eslint-disable no-unused-vars */
+// import React, { useState } from "react";
+// import {
+//   TextField,
+//   Typography,
+//   Button,
+//   Box,
+//   Grid,
+//   CircularProgress,
+// } from "@mui/material";
 
-const ReviewForm = ({ onReviewSubmit, loading }) => {
+// const ReviewForm = ({ onReviewSubmit, loading }) => {
+//   const [guestRating, setGuestRating] = useState(0);
+//   const [comment, setComment] = useState("");
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const newReview = {
+//       guestRating,
+//       comment,
+//     };
+//     onReviewSubmit(newReview);
+//     setGuestRating(0);
+//     setComment("");
+//   };
+
+//   return (
+//     <Box sx={{ maxWidth: 400, mx: "auto" }}>
+//       <Typography variant="h6" gutterBottom>
+//         Write a Review
+//       </Typography>
+//       <form onSubmit={handleSubmit}>
+//         <TextField
+//           type="number"
+//           label="Rating (1-5)"
+//           value={guestRating}
+//           onChange={(e) => {
+//             // Limit the guestRating to be between 1 and 5
+//             let value = parseFloat(e.target.value);
+//             if (value < 1) {
+//               value = 1;
+//             } else if (value > 5) {
+//               value = 5;
+//             }
+//             setGuestRating(value);
+//           }}
+//           fullWidth
+//           required
+//           inputProps={{
+//             min: 1, // Minimum value
+//             max: 5, // Maximum value
+//             step: 0.1, // Decimal step value
+//           }}
+//           sx={{
+//             my: 2,
+//           }}
+//         />
+//         <TextField
+//           label="Comment"
+//           value={comment}
+//           onChange={(e) => setComment(e.target.value)}
+//           fullWidth
+//           multiline
+//           required
+//         />
+//         <Grid container justifyContent="center">
+//           <Button
+//             type="submit"
+//             variant="contained"
+//             color="primary"
+//             sx={{ mt: 2, background: "#8DD3BB" }}
+//             disabled={loading}
+//           >
+//             {loading ? (
+//               <>
+//                 Submit Review{" "}
+//                 <CircularProgress
+//                   size={24}
+//                   sx={{
+//                     verticalAlign: "middle",
+//                     ml: 1,
+//                     color: "white",
+//                     width: "1.5rem",
+//                   }}
+//                 />
+//               </>
+//             ) : (
+//               "Submit Review"
+//             )}
+//           </Button>
+//         </Grid>
+//       </form>
+//     </Box>
+//   );
+// };
+
+// export default ReviewForm;
+
+
+
+
+// components/ReviewForm.js
+import React, { useState } from 'react';
+import { TextField, Typography, Button, Box, Grid } from '@mui/material';
+
+const ReviewForm = ({ onReviewSubmit }) => {
+  const [user, setUser] = useState('');
   const [guestRating, setGuestRating] = useState(0);
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newReview = {
+      user,
       guestRating,
       comment,
     };
     onReviewSubmit(newReview);
-    setGuestRating(0);
-    setComment("");
+    setUser('');
+    setGuestRating
+(0);
+    setComment('');
   };
 
   return (
-    <Box sx={{ maxWidth: 400, mx: "auto" }}>
-      <Typography variant="h6" gutterBottom>
+    <Box sx={{ maxWidth: 400, mx: 'auto' }}>
+      <Typography variant="review2" gutterBottom>
         Write a Review
       </Typography>
       <form onSubmit={handleSubmit}>
+        <Typography variant='review1'>
+        {/* <TextField
+          label="Name"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          fullWidth
+          required
+        /> */}<br/>
         <TextField
           type="number"
-          label="Rating (1-5)"
+          label="guestRating"
           value={guestRating}
           onChange={(e) => {
             // Limit the guestRating to be between 1 and 5
-            let value = parseFloat(e.target.value);
+            let value = e.target.value;
             if (value < 1) {
               value = 1;
             } else if (value > 5) {
               value = 5;
             }
-            setGuestRating(value);
+            setGuestRating
+        (value);
           }}
           fullWidth
           required
@@ -50,10 +162,7 @@ const ReviewForm = ({ onReviewSubmit, loading }) => {
             max: 5, // Maximum value
             step: 0.1, // Decimal step value
           }}
-          sx={{
-            my: 2,
-          }}
-        />
+        /><br/><br/>
         <TextField
           label="Comment"
           value={comment}
@@ -62,33 +171,14 @@ const ReviewForm = ({ onReviewSubmit, loading }) => {
           multiline
           required
         />
-        <Grid container justifyContent="center">
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2, background: "#8DD3BB" }}
-            disabled={loading}
-          >
-            {loading ? (
-              <>
-                Submit Review{" "}
-                <CircularProgress
-                  size={24}
-                  sx={{
-                    verticalAlign: "middle",
-                    ml: 1,
-                    color: "white",
-                    width: "1.5rem",
-                  }}
-                />
-              </>
-            ) : (
-              "Submit Review"
-            )}
-          </Button>
+        </Typography>
+        <Grid container justifyContent='center'>
+        <Button type="submit" variant="contained" color="primary" sx={{ mt: 2, background:'#8DD3BB' }}>
+          Submit Review
+        </Button>
         </Grid>
       </form>
+      <Box sx={{width:123, height:60}}></Box>
     </Box>
   );
 };
