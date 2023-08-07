c3c1a76a95b18db7740ea26fb8a025fb50f9443f
Shakithya <shakithya05@gmail.com>
2023-08-07 11:13:36 +0530

    booking

--- src/components/FinalBookingDetails/Details.jsx
+++ src/components/FinalBookingDetails/Details.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Box } from '@mui/material'
+import {Container} from '@mui/material'
 import Detail from './TitleCard';
 import Booking from './Booking'
 import Terms from './Terms'
-import Top from '../hotel-details/images-display/Top';
 
 const Main = () => {
-    const bookingDetails = {
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        checkInDate: '2023-08-10',
-        checkOutDate: '2023-08-15',
-        roomType: 'Deluxe Room',
-        checkInTime: '8.00am',
-        checkOutTime: '9.00pm',
-        roomNumber: 'onarrival',
-    };
+  
 
-    return (
-        <>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '0px 10%' }}>
-                <Top />
-                <Detail />
-                <Booking bookingDetails={bookingDetails} />
-                <Terms />
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Container id="pageContent" style={{paddingTop: '6rem'}}>
+      <Detail/>
+      <Booking  />
+      <Terms />
+      </Container>
+      
+    </>
+  );
 };
 
 export default Main;
+
+// const bookingDetails = {
+  //   name: 'John Doe',
+  //   email: 'john.doe@example.com',
+  //   checkInDate: '',
+  //   checkOutDate: '',
+  //   roomType: 'Deluxe Room',
+  //   checkInTime: '8.00am',
+  //   checkOutTime: '9.00pm',
+  //   roomNumber: 'onarrival',
+  // };
